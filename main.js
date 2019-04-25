f9b687ff120e78c2f85953461170457b9a3cab0a
Vitya Drykin <pepperforcemeat@outlook.com>
2019-04-25 23:21:33 +0300

    start

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let getRandom = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+    let genBtn = document.getElementById("genBtn");
+    genBtn.addEventListener("click", randoming);
+
+    function randoming() {
+        let x1 = document.getElementById("x1").value,
+            y1 = document.getElementById("y1").value,
+            x2 = document.getElementById("x2").value,
+            y2 = document.getElementById("y2").value,
+            x3 = document.getElementById("x3").value,
+            y3 = document.getElementById("y3").value,
+            x4 = document.getElementById("x4").value,
+            y4 = document.getElementById("y4").value;
+        
+        let first = getRandom(x1, y1);
+        let second = first + getRandom(x2, y2);
+        let third = second + getRandom(x3, y3);
+        let fourth = third + getRandom(x4, y4);
+        
+        console.log(first, second, third, fourth);
+    }
+
+})
+
