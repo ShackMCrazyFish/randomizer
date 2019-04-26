65b5b52e82e4a900dcbce37b08b1099b9b1d00e3
Vitya Drykin <pepperforcemeat@outlook.com>
2019-04-26 21:28:28 +0300

    add chart

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     
     let getRandom = (min, max) => Math.floor(Math.random() * (parseInt(max) - parseInt(min) + 1) ) + parseInt(min);
 
-    let genBtn = document.getElementById("genBtn"),
-        result = document.getElementById("result");
+    let genBtn = document.getElementById("genBtn");
     genBtn.addEventListener("click", randoming);
 
     function randoming() {
@@ -24,7 +23,35 @@ document.addEventListener("DOMContentLoaded", function() {
         let fourth = third + getRandom(x4, y4);
         
         result.innerHTML = first + " " + second + " " + third + " " + fourth;
+        drawChart(first, second, third, fourth);
+    }
+
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    
+    function drawChart(first, second, third, fourth) {
+        var data = google.visualization.arrayToDataTable([
+            ['Index', 'Value'],
+            ['1', first],
+            ['2', second],
+            ['3', third],
+            ['4', fourth]
+        ]);
+    
+        var options = {
+            title: 'Company Performance',
+            curveType: 'function',
+            legend: {
+                position: 'bottom'
+            }
+        };
+    
+        var chart = new google.visualization.LineChart(document.getElementById('chart'));
+    
+        chart.draw(data, options);
     }
 
 })
+
 
